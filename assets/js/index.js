7e6f9b17188ff426f70da4f09959188114e47cea
Arleigh Mitchell <arleighmitchell@gmail.com>
2016-09-15 12:21:58 -0700

    Create digital clock via functions on js file

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,3 +19,29 @@ var background = function() {
     document.getElementById("h1").innerText = "Good Evening!";
   };
 };
+
+background();
+
+var startclock = function() {
+  var today = new Date();
+  var hours = today.getHours();
+  var minutes = today.getMinutes();
+  var seconds = today.getSeconds();
+  m = checkTime(minutes);
+  s = checkTime(seconds);
+  var time = setTimeout(startclock, 1000);
+
+  document.getElementById("txt").innerText = hours + ":" + minutes + ":" + seconds;
+};
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+    return i;
+  };
+};
+
+startclock();
+
+checkTime(m);
+checkTime(s);
